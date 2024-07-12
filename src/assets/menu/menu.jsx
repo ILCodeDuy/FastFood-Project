e6064e07473194a7407a4a2fb70526bdf22311ed
duy <vdntduy0410@gmail.com>
2024-07-13 01:31:31 +0700

    feat: load img

---
+++ src/assets/menu/menu.jsx
@@ -0,0 +1,109 @@
+import img1 from '../img/b1_cahoigionvothantre.jpg';
+import img2 from '../img/b2_bonuongwhopper.jpg';
+import img3 from '../img/b3_cagion.jpg';
+import img4 from '../img/b4_bonuonghanhchien.jpg';
+import img5 from '../img/b5_gagioncay.jpg';
+import img6 from '../img/b6_ganuong.jpg';
+import img7 from '../img/b7_botamphomai.jpg';
+import img8 from '../img/b8_bophomaithitxongkhoi.jpg';
+import img9 from '../img/b9_bonuongphomaijpg.jpg';
+import img10 from '../img/b10_gaphomaisitbbq.jpg';
+import img11 from '../img/b11_whopperphomaithitxongkhoi.jpg';
+import img12 from '../img/b12_doublewhopper.jpg';
+import img13 from '../img/b13_2miengbonuongphomai.jpg';
+import img14 from '../img/b14_2miengbophomaithitxongkhoi.jpg';
+import img15 from '../img/c1_1mienggabbq.jpg';
+import img16 from '../img/c2_1mienggagioncrispy.jpg';
+import img17 from '../img/c3_1mienggaflamingioncay.jpg';
+import img18 from '../img/c4_mixwing2pcs.jpg';
+import img19 from '../img/c5_mixwing6pcs.jpg';
+import img20 from '../img/c6_mixwing4pcs.jpg';
+import img21 from '../img/c7_cb3canhgabbq_khoaitaychien_1douong.jpg';
+import img22 from '../img/c8_cb3mienggarangioncay_1khoaitaychien_1nuoc.jpg';
+import img23 from '../img/c9_cb2canhgabbq_1khoaitaychien_1douong.jpg';
+import img24 from '../img/c10_cb2gagionkhongcay_1khoaitaychien_1douong.jpg';
+import img25 from '../img/c11_2mienggagioncay_1khoaitaychien_1nuoc.jpg';
+import img26 from '../img/c12_cbgagionkhongcay_1khoaitaychien_1douong.jpg';
+import img27 from '../img/p1_phomaitruyenthong_cheesemania.jpg';
+import img28 from '../img/p2_dambong_dua_kieuhawaii.jpg';
+import img29 from '../img/p3-Ga-Xot-Tuong-Kieu-Nhat-Teriyaki-Chicken.jpg';
+import img30 from '../img/p4_Pepperoni-feast-Pizza-Xuc-Xich-Y.jpg';
+import img31 from '../img/p5_Meat-lover-Pizza-5-Loai-Thit-Thuong-Hang.jpg';
+import img32 from '../img/p6_Veggie-mania-Pizza-Rau-Cu-Thap-Cam.jpg';
+import img33 from '../img/p7_Pizza-Thap-Cam-Thuong-Hang-Extravaganza.jpg';
+import img34 from '../img/p8_ngapviphomaihaohang_cheesymadness.jpg';
+import img35 from '../img/p9_Pizza-Ga-Pho-Mai-Thit-Heo-Xong-Khoi-Cheesy-Chicken-Bacon.jpg';
+import img36 from '../img/p10_xucxichphomai_sausagekidmania.jpg';
+import img37 from '../img/p11_Pizza-Hai-San-Xot-Ca-Chua-Seafood-Delight.jpg';
+import img38 from '../img/p12_Pizzaminsea-Hai-San-Nhiet-Doi-Xot-Tieu.jpg';
+import img39 from '../img/p13_Pizza-Hai-San-Xot-Mayonnaise-Ocean-Mania.jpg';
+import img40 from '../img/p14Surf-turf-Pizza-Bo-Tom-Nuong-Kieu-My-1.jpg';
+import img41 from '../img/d1_chaicoca-cola-390ml.jpg';
+import img42 from '../img/d2_fanta-390.jpg';
+import img43 from '../img/d3_Sprite-390.jpg';
+import img44 from '../img/d4_cocacola-coke-zero.jpg';
+import img45 from '../img/d5_coke-1.5.jpg';
+import img46 from '../img/d6_zerosugar-coke-1.5-new-new.png';
+import img47 from '../img/d7_Sprite-1.5L.jpg';
+import img48 from '../img/d8_fanta-1.5.jpg';
+import img49 from '../img/d9_tra-dao-hat-chia.jpg';
+import img50 from '../img/d10_Fuzetea+-Chanh-Day-hatchia-350ml-new-new.png';
+import img51 from '../img/d11_daisani.jpg';
+import img52 from '../img/d12_dasani-1l5-new.png';
+
+const imagePaths = {
+    'b1_cahoigionvothantre.jpg': img1,
+    'b2_bonuongwhopper.jpg': img2,
+    'b3_cagion.jpg': img3,
+    'b4_bonuonghanhchien.jpg': img4,
+    'b5_gagioncay.jpg': img5,
+    'b6_ganuong.jpg': img6,
+    'b7_botamphomai.jpg': img7,
+    'b8_bophomaithitxongkhoi.jpg': img8,
+    'b9_bonuongphomaijpg.jpg': img9,
+    'b10_gaphomaisitbbq.jpg': img10,
+    'b11_whopperphomaithitxongkhoi.jpg': img11,
+    'b12_doublewhopper.jpg': img12,
+    'b13_2miengbonuongphomai.jpg': img13,
+    'b14_2miengbophomaithitxongkhoi.jpg': img14,
+    'c1_1mienggabbq.jpg': img15,
+    'c2_1mienggagioncrispy.jpg': img16,
+    'c3_1mienggaflamingioncay.jpg': img17,
+    'c4_mixwing2pcs.jpg': img18,
+    'c5_mixwing6pcs.jpg': img19,
+    'c6_mixwing4pcs.jpg': img20,
+    'c7_cb3canhgabbq_khoaitaychien_1douong.jpg': img21,
+    'c8_cb3mienggarangioncay_1khoaitaychien_1nuoc.jpg': img22,
+    'c9_cb2canhgabbq_1khoaitaychien_1douong.jpg': img23,
+    'c10_cb2gagionkhongcay_1khoaitaychien_1douong.jpg': img24,
+    'c11_2mienggagioncay_1khoaitaychien_1nuoc.jpg': img25,
+    'c12_cbgagionkhongcay_1khoaitaychien_1douong.jpg': img26,
+    'p1_phomaitruyenthong_cheesemania.jpg': img27,
+    'p2_dambong_dua_kieuhawaii.jpg': img28,
+    'p3-Ga-Xot-Tuong-Kieu-Nhat-Teriyaki-Chicken.jpg': img29,
+    'p4_Pepperoni-feast-Pizza-Xuc-Xich-Y.jpg': img30,
+    'p5_Meat-lover-Pizza-5-Loai-Thit-Thuong-Hang.jpg': img31,
+    'p6_Veggie-mania-Pizza-Rau-Cu-Thap-Cam.jpg': img32,
+    'p7_Pizza-Thap-Cam-Thuong-Hang-Extravaganza.jpg': img33,
+    'p8_ngapviphomaihaohang_cheesymadness.jpg': img34,
+    'p9_Pizza-Ga-Pho-Mai-Thit-Heo-Xong-Khoi-Cheesy-Chicken-Bacon.jpg': img35,
+    'p10_xucxichphomai_sausagekidmania.jpg': img36,
+    'p11_Pizza-Hai-San-Xot-Ca-Chua-Seafood-Delight.jpg': img37,
+    'p12_Pizzaminsea-Hai-San-Nhiet-Doi-Xot-Tieu.jpg': img38,
+    'p13_Pizza-Hai-San-Xot-Mayonnaise-Ocean-Mania.jpg': img39,
+    'p14Surf-turf-Pizza-Bo-Tom-Nuong-Kieu-My-1.jpg': img40,
+    'd1_chaicoca-cola-390ml.jpg': img41,
+    'd2_fanta-390.jpg': img42,
+    'd3_Sprite-390.jpg': img43,
+    'd4_cocacola-coke-zero.jpg': img44,
+    'd5_coke-1.5.jpg': img45,
+    'd6_zerosugar-coke-1.5-new-new.png': img46,
+    'd7_Sprite-1.5L.jpg': img47,
+    'd8_fanta-1.5.jpg': img48,
+    'd9_tra-dao-hat-chia.jpg': img49,
+    'd10_Fuzetea+-Chanh-Day-hatchia-350ml-new-new.png': img50,
+    'd11_daisani.jpg': img51,
+    'd12_dasani-1l5-new.png': img52,
+  };
+
+export default imagePaths;
--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -1,42 +1,55 @@
-import React from "react";
-import { anh } from "../../assets/img";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-
+import imagePaths from "../../assets/menu/menu";
+  
 const Menu = () => {
-    const plus = faPlus;
+  const plus = faPlus;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/data")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data); // Lưu kết quả từ API vào state products
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []); // Sử dụng mảng rỗng để chỉ gọi API một lần sau khi component được render
   return (
     <div className="container mx-auto px-4">
-        <h2 className="text-center dancing-script text-slate-950 text-5xl">
-          Our Menu{" "}
-        </h2>
-        <div className="mt-24 mx-12 flex flex-wrap">
-          <div className="p-6 mx-12 mt-6 h-box w-72 rounded-2xl bg-gray-900 text-white">
-            <img
-              className="w-64 h-48 rounded-3xl hover:scale-105"
-              src={anh}
-              alt=""
+      <h2 className="text-center dancing-script text-slate-950 text-5xl">
+        Our Menu{" "}
+      </h2>
+      <div className="mt-24 mx-12 flex flex-wrap">
+      {products.map(product => (
+        <div key={product.id} className="p-6 mx-12 mt-6 h-box w-72 rounded-2xl bg-gray-900 text-white">
+          <img
+            className="w-64 h-48 rounded-3xl hover:scale-105"
+            src={imagePaths[product.img]} alt={product.name}
+          />
+          <h2 className="text-3xl dancing-script mt-4 mb-10">{product.name}</h2>
+          <span className="font-bold mt-20">{product.price} VNĐ</span>
+          <p className="font-bold mt-4">Số lượng còn lại: {product.quantity}</p>
+          <button className="bg-yellow-400 p-3 rounded-full w-12 relative left-48 bottom-16">
+            <FontAwesomeIcon
+              icon={plus}
+              className="text-white text-xl text-center cursor-pointer"
             />
-            <h2 className="text-3xl dancing-script mt-4 mb-10">2</h2>
-            <span className="font-bold mt-20"> VNĐ</span>
-            <p className="font-bold mt-4">Số lượng còn lại:</p>
-            <button className="bg-yellow-400 p-3 rounded-full w-12 relative left-48 bottom-16">
-              <FontAwesomeIcon
-                icon={plus}
-                className="text-white text-xl text-center cursor-pointer"
-              />
-            </button>
-            <button className="bg-teal-800 border-2 p-1 border-slate-950 hover:border-gray-100 rounded-full w-32 mt-6">
-              Chi tiết
-            </button>
-          </div>
-        </div>
-        <div className="text-center">
-          <button className="btn mt-6 bg-gray-800 text-white hover:text-black">
-            Xem Thêm
+          </button>
+          <button className="bg-teal-800 border-2 p-1 border-slate-950 hover:border-gray-100 rounded-full w-32 mt-6">
+            Chi tiết
           </button>
         </div>
+      ))}
       </div>
+      <div className="text-center">
+        <button className="btn mt-6 bg-gray-800 text-white hover:text-black">
+          Xem Thêm
+        </button>
+      </div>
+    </div>
   );
 };
 
